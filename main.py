c3993286eb4e3bc4dd8203812d97c7b4e97653af
noahkurz <55148190+noahkurz@users.noreply.github.com>
2021-02-05 20:27:40 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,27 +2,23 @@ import requests
 import bs4 as bs
 from splinter import Browser
 import helpers
+import time
 
 #build the URL to attack... 
-class supremeBot(object):
+class shoppingBot(object):
     def __init__(self, **info):
         self.base_url = 'https://www.bestbuy.com/site/sony-playstation-5-dualsense-wireless-controller/6430163.p?skuId=6430163'
         self.login_url = 'https://www.bestbuy.com/identity/global/signin'
         self.checkout_url = 'https://www.bestbuy.com/checkout/r/fulfillment'
-        #self.product = 'site/sony-playstation-5-console/6426149.p?skuId=6426149'
-        #self.checkout = '/checkout'
         self.info = info
-#this is all good...
-    def initializeBrowser(self):
+
+    def startBrowser(self):
         driver = self.info["driver"]
         path = helpers.get_driver_path(driver)
         if driver == "geckodriver":
             self.b = Browser()
         elif driver == "chromedriver":
-            executable_path = {"executable_path": path}
-            self.b = Browser('chrome', **executable_path)
-#So this is to find multiple products(in my case, so lets skip this for now)
-#TO DO...
+            print("Chrome is currently not supported...coming soon... working out a few bugs")
 
     def login(self):
         self.b.visit(
@@ -30,11 +26,11 @@ class supremeBot(object):
                 self.login_url
             )
         )
-        self.b.find_by_xpath("//*[@id='fld-e']").fill("noahkurz19@gmail.com")
-        self.b.find_by_xpath("//*[@id='fld-p1']").fill("Sabers7168!")
+        self.b.find_by_xpath("//*[@id='fld-e']").fill("")#enter ypur emai;
+        self.b.find_by_xpath("//*[@id='fld-p1']").fill("")#enter your best buy password associated with the email.
         self.b.find_by_xpath("/html/body/div[1]/div/section/main/div[1]/div/div/div/div/form/div[4]/button").click()
     
-    def visitSite(self):
+    def allocateProduct(self):
         self.b.visit(
             "{}".format(
                 self.base_url))
@@ -42,27 +38,27 @@ class supremeBot(object):
         try:
             self.b.find_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[8][1]").click()
             self.b.find_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[8][1]").click()
-            self.b.find_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[8][1]").click()
-            
+            self.b.find_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[8][1]").click()   
         except:
-            timeout = 45
-            self.b.visit(
-            "{}".format(
-                self.base_url))
-            
+            time.sleep(4)
             self.b.find_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[8][1]").click()
             self.b.find_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[8][1]").click()
             self.b.find_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[8][1]").click()
+    
     def checkout(self):
-
         self.b.visit("{}".format(self.checkout_url))
         self.b.visit("{}".format(self.checkout_url))
         self.b.find_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div[1]/div[1]/main/div[2]/div[2]/form/section/div/div[2]/div/div/button/span").click()
 
-
     def submitPayment(self):
-        #self.b.find_by_xpath("//*[@id='credit-card-cvv']").fill("137")
-        self.b.find_by_xpath("//*[@id='credit-card-cvv']").fill("137")
+        CCVCode = ""
+        try:
+            self.b.find_by_xpath("//*[@id='credit-card-cvv']").fill(CCVCode)#enter the CCV code associated with your best buy account
+        except:
+            time.sleep(2)
+            self.b.find_by_xpath("//*[@id='credit-card-cvv']").fill(CCVCode)#enter the CCV code associated with your best buy account
+        print("we made it to the end!")
+        #self.b.find_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div[1]/div[1]/main/div[2]/div[3]/div/section/div[4]/button").click()
         
         
 
@@ -73,22 +69,16 @@ if __name__ == "__main__":
     INFO = {
         "driver": "geckodriver"
     }
-    BOT = supremeBot(**INFO)
-    # Flag to set to true if you want to reload the page continously close to drop.
-    #found_product = False
-   # max_iter = 10
-   # counter = 1
-    #while not found_product and counter < max_iter:
-    #    found_product = BOT.findProduct()
-    #    print("Tried ", counter, " times")
-    #    counter += 1
-    #if not found_product:
-    #    raise Exception("Couldn't find product. Sry bruhhhh")
-    BOT.initializeBrowser()
-
+    BOT = shoppingBot(**INFO)
+    print("Hello, buying Bot at your service!...\n")
+    print("Bot starting browser now...\n")
+    BOT.startBrowser()
+    print("Bot is now starting the process of securing a ps5...\n")
     BOT.login()
-    timeout = 45
-    #BOT.visitSite()
-    timeout = 45
+    print("Bot is logging in...\n")
+    BOT.allocateProduct()
+    print("Bot is trying to add product to cart...\n")
     BOT.checkout()
+    print("Bot is starting checkout process...\n")
     BOT.submitPayment()
+    print("Bot confirms that ps5 is secured.")
